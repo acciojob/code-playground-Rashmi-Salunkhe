b502571deb7ee28c1705841379a45dfdcd2f12cf
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-11-04 19:41:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Ensure this matches the path and export
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
